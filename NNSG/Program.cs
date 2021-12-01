a0d27211d3a598f1612d9fa93647b4b3badbf720
gabriel rossier <rossier.gabriel218@gmail.com>
2021-12-01 11:25:40 +0100

    Added default UI function

--- NNSG/Program.cs
+++ NNSG/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
-
+using System.Threading;
 namespace NNSG
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Read();
+            Thread th = new Thread(new ThreadStart(bruh));
+            th.Start();
+            UI ui = UI.getInstance();
+            ui.RenderLogo();
+            ui.Write("\n\n" + "To Start a game presse [1]");
+            string input = ui.Read();
+
+            
+
+            if (input == "1")
+            {
+                GameInstance();
+            }
+        }
+        static void bruh()
+        {
+            while (true)
+            {
+                Console.WriteLine("bruh");
+                Thread.Sleep(1000);
+            }
+        }
+        static void GameInstance()
+        {
+            while (false)
+            {
+                //TODO
+            }
         }
     }
 }
--- NNSG/UI.cs
+++ NNSG/UI.cs
@@ -6,5 +6,58 @@ namespace NNSG
 {
     class UI
     {
+        private static UI instance;
+        const string logo = @"
+        ███╗   ██╗ ██████╗     ███╗   ██╗ █████╗ ███╗   ███╗███████╗    ███████╗███████╗██████╗ ██╗ ██████╗ ██╗   ██╗███████╗     ██████╗  █████╗ ███╗   ███╗███████╗
+        ████╗  ██║██╔═══██╗    ████╗  ██║██╔══██╗████╗ ████║██╔════╝    ██╔════╝██╔════╝██╔══██╗██║██╔═══██╗██║   ██║██╔════╝    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝
+        ██╔██╗ ██║██║   ██║    ██╔██╗ ██║███████║██╔████╔██║█████╗      ███████╗█████╗  ██████╔╝██║██║   ██║██║   ██║███████╗    ██║  ███╗███████║██╔████╔██║█████╗  
+        ██║╚██╗██║██║   ██║    ██║╚██╗██║██╔══██║██║╚██╔╝██║██╔══╝      ╚════██║██╔══╝  ██╔══██╗██║██║   ██║██║   ██║╚════██║    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  
+        ██║ ╚████║╚██████╔╝    ██║ ╚████║██║  ██║██║ ╚═╝ ██║███████╗    ███████║███████╗██║  ██║██║╚██████╔╝╚██████╔╝███████║    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗
+        ╚═╝  ╚═══╝ ╚═════╝     ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝    ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝ ╚═════╝  ╚═════╝ ╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+                                                                                                                                                             
+        ";
+        private UI()
+        {
+
+        }
+
+        public static UI getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UI();
+            }
+            return instance;
+        }
+
+        public void RenderLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(logo);
+        }
+
+
+        public void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+
+        public string Read()
+        {
+            return Console.ReadLine(); ;
+        }
+
+        /// <summary>
+        /// Render a list of choices 
+        /// </summary>
+        /// <param name="choices">Dictionnary of key and string to choice</param>
+        public void ChoiceRender(Dictionary<string,string> choices)
+        {
+            foreach (var item in choices)
+            {
+                Console.WriteLine(item.Value + "["+item.Key+"]");
+            }
+        }
     }
 }
