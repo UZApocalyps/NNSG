e9cc0473458612185513301fdc4c2bb537d66b30
gabriel rossier <rossier.gabriel218@gmail.com>
2021-12-02 11:18:03 +0100

    Added function StartGame

---
+++ NNSG/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NNSG.Needs;
+using NNSG.Jobs;
+namespace NNSG
+{
+    public class GameManager
+    {
+        private static GameManager instance;
+        private GameManager()
+        {
+            //TODO instanciate objects
+        }
+
+        public static GameManager GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+
+        public void StartGame()
+        {
+            List<Person> people = new List<Person>();
+            //Instanciate Goods
+            Good food = new Good();
+            food.name = "food";
+            food.type = GoodType.Food;
+            food.ammount = 100;
+            food.price = 1;
+            
+            //Instanciate Job
+            Job job = new Job(GoodType.Food);
+            job.Name = "Farmer";
+            job.persons = new List<Person>();
+            job.quantityPerTick = 1;
+
+            //Instanciate Person
+
+            for (int i = 0; i < 100; i++)
+            {
+                Person person = new Person();
+                person.id = new Random().Next(0,int.MaxValue);
+                person.age = new Random().Next(10, 50);
+                if (i < 30)
+                {
+                    person.job = job;
+                    job.persons.Add(person);
+                }
+                people.Add(person);
+            }
+
+            //Instanciate Needs
+
+            foreach (var person in people)
+            {
+                Hunger hunger = new Hunger();
+                person.needs = new Need[1];
+                person.needs[(int)NeedsType.hunger] = hunger;
+            }
+
+            //Instanciate Time
+
+        }
+    }
+}
