d9f10ce0553bc2529becd3628fa1026dfca45642
gabriel rossier <rossier.gabriel218@gmail.com>
2021-12-02 11:29:08 +0100

    Remove useless tests

--- NNSG/Program.cs
+++ NNSG/Program.cs
@@ -7,27 +7,17 @@ namespace NNSG
 
         static void Main(string[] args)
         {
-            Thread th = new Thread(new ThreadStart(bruh));
-            th.Start();
+           
             UI ui = UI.getInstance();
+
             ui.RenderLogo();
             ui.Write("\n\n" + "To Start a game presse [1]");
             string input = ui.Read();
-
-            
 
             if (input == "1")
             {
                 GameManager gm = GameManager.GetInstance();
                 gm.StartGame();
-            }
-        }
-        static void bruh()
-        {
-            while (true)
-            {
-                Console.WriteLine("bruh");
-                Thread.Sleep(1000);
             }
         }
     }
