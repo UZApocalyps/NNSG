2756d9ba79bfb196b26ac398b66e53e1d81273e4
WilliamWHN <william.hausmann@cpnv.ch>
2022-01-19 11:22:27 +0100

    Refactor goods

---
+++ NNSG/Goods/Good.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NNSG.Goods
+{
+    abstract class Good
+    {
+        public float price;
+        public float amount;
+    }
+}
