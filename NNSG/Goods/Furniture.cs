50bbe67bd894bdf2a8ee7b6ff42e677e100b04d7
WilliamWHN <william.hausmann@cpnv.ch>
2022-01-19 11:35:30 +0100

    Add goods

---
+++ NNSG/Goods/Furniture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NNSG.Goods
+{
+    class Furniture : Good
+    {
+        public Furniture(float amountValue, float priceValue)
+        {
+            amount = amountValue;
+            price = priceValue;
+        }
+    }
+}
