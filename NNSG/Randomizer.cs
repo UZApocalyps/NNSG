5ed0026a98e0c3d95fb215c1e94df9e2c605a1d4
Quentin Neves <neves.quentin@gmail.com>
2022-01-21 07:51:19 +0100

    Add comment in Randomizer.cs

--- NNSG/Randomizer.cs
+++ NNSG/Randomizer.cs
@@ -8,13 +8,24 @@ namespace NNSG
     {
         private static Random randNumber = new Random();
 
+        /// <summary>
+        /// Return true if random value generated is smaller than probability
+        /// </summary>
+        /// <param name="probability">value between 0 and 100</param>
+        /// <returns></returns>
         public static bool Probability(float probability)
         {
             int number = randNumber.Next(0, 100);
             return number < probability;
         }
 
-        public static int Range(int from, int to) //To is exclusive
+        /// <summary>
+        /// Returns a int between given values
+        /// </summary>
+        /// <param name="from">Minimal value</param>
+        /// <param name="to">Maximum value (exclusive)</param>
+        /// <returns>Random int</returns>
+        public static int Range(int from, int to)
         {
             return randNumber.Next(from, to);
         }
