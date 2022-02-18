19998b4f8e709083379ed227d69b924fd53fb430
Quentin Neves <neves.quentin@gmail.com>
2022-02-18 08:41:59 +0100

    Add description to Job.cs

--- NNSG/Jobs/Job.cs
+++ NNSG/Jobs/Job.cs
@@ -5,9 +5,11 @@ using System.Text;
 namespace NNSG.Jobs
 {
     abstract class Job : ITick
-    {
+    {        
+        /// <summary>
+        /// How much resource is produced each tick
+        /// </summary>
         public int quantityPerTick;
         public abstract void Ticking();
-
     }
 }
