0456527a93b9ce5e62ecee6a217e9b044c584d73
Quentin Neves <neves.quentin@gmail.com>
2022-03-04 13:32:41 +0100

    Create logger class

---
+++ NNSG/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Timers;
+namespace NNSG
+{
+    /// <summary>
+    /// Keep track of each day
+    /// </summary>
+    class Logger
+    {
+        private static Logger instance;
+        /// <summary>
+        /// Return the single instance of this class
+        /// </summary>
+        /// <returns>Singleton of Logger</returns>
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+    }
+}
